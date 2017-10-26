486d8846db5e0905223c74bbea663fcf06788a2c
Enzo Cornand <goulaheau@gmail.com>
2017-10-26 16:25:51 +0200

    Camera

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { CameraPage } from "../pages/camera/camera";
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { Camera } from "@ionic-native/camera";
+import { Base64ToGallery } from "@ionic-native/base64-to-gallery";
 
 @NgModule({
   declarations: [
@@ -33,7 +34,8 @@ import { Camera } from "@ionic-native/camera";
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     StatusBar,
     SplashScreen,
-    Camera
+    Camera,
+    Base64ToGallery
   ]
 })
 export class AppModule {}
--- src/pages/camera/camera.ts
+++ src/pages/camera/camera.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Camera, CameraOptions } from '@ionic-native/camera';
+import { Base64ToGallery } from "@ionic-native/base64-to-gallery";
 
 @Component({
   selector: 'page-camera',
@@ -10,7 +11,8 @@ export class CameraPage {
 
   private options: CameraOptions;
 
-  constructor(private camera: Camera) {
+  constructor(private camera: Camera,
+              private base64ToGallery: Base64ToGallery) {
     this.options = {
       quality: 100,
       destinationType: this.camera.DestinationType.DATA_URL,
@@ -25,5 +27,12 @@ export class CameraPage {
     }, (err) => {
       console.log(err);
     });
+  }
+
+  saveInGallery() {
+    this.base64ToGallery.base64ToGallery(this.base64Image, { prefix: '_img' }).then(
+      res => console.log('Saved image to gallery ', res),
+      err => console.log('Error saving image to gallery ', err)
+    );
   }
 }
